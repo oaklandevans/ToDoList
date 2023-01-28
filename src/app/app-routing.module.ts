3e21cbf0d08431a4303fbb7fd73a653c41289bb8
jisav7t <oakland.evans@jbhunt.com>
2023-01-27 21:40:25 -0600

    ToDoList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [{
+    path: 'todo',
+    loadChildren: () => import('./todo-list/todo-list.module').then(m => m.TodoListModule)
+  }]
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/todo-list/models/task.interface.ts
@@ -0,0 +1,10 @@
+export enum TaskType {
+  pending,
+  complete
+}
+
+export interface Task {
+  status: TaskType;
+  description: string;
+  id: number;
+}
